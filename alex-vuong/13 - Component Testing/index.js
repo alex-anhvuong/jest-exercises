5c955569cf54f2e6af2bb434968c83380693fcd4
Alex Vuong <vavuong@deakin.edu.au>
2021-06-05 13:21:03 +1000

    13 Component testing, React

--- alex-vuong/13 - Component Testing/index.js
+++ alex-vuong/13 - Component Testing/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default () => {
+export default (props) => {
   // Implement this component to pass the tests in ./__tests/index.spec.js
+  const listItems = props.data.map((d) => {
+    const item = <li
+      key={d.key}
+      onClick={() => props.onClick(d.key)}
+      className={d.selected ? "selected" : ""}
+    >
+      {d.name}
+    </li>
+    return item;
+  });
+
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
 };
