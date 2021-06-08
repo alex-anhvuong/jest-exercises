fb9b7a373e88420a7d84497ba5f94c629795f177
Alex Vuong <vavuong@deakin.edu.au>
2021-06-08 14:07:50 +1000

    Fetch and resolve, deep camelCase of json response

--- alex-vuong/14 - Promise Testing/index.js
+++ alex-vuong/14 - Promise Testing/index.js
@@ -4,9 +4,9 @@ export const LOCATION_ORIGIN = (() => {
   if (typeof window !== 'undefined' && window.location !== 'undefined') {
     const url = window.location.origin ||
       window.location.protocol +
-        '//' +
-        window.location.hostname +
-        (window.location.port ? ':' + window.location.port : '');
+      '//' +
+      window.location.hostname +
+      (window.location.port ? ':' + window.location.port : '');
     if (url.toString() !== 'null') {
       return url;
     }
@@ -24,14 +24,50 @@ export const defaultFetchHeaders = {
 
 export const camelCase = input => {
   // TODO: implement
+  const firstChar = input.charAt(0);
+  return input.replace(firstChar, firstChar.toLowerCase());
 };
 
 const normalizeCasing = value => {
-  // TODO: implement
+  //  If value is an array
+  if (Array.isArray(value)) {
+    for (const e of value)
+      normalizeCasing(e);
+  }
+  //  If value is an object
+  //  look into value's keys
+  else if (typeof value == "object") {
+    // camelCase for all propeties in value
+    // and normalize the value of each key
+    for (const property in value) {
+      const camelCaseKey = camelCase(property);
+
+      //  camelCase the property if it hasn't been cameled
+      if (camelCaseKey != property) {
+        value[camelCaseKey] = value[property];
+        delete value[property];
+      }
+
+      //  normalise the value of the property
+      normalizeCasing(value[camelCaseKey]);
+    }
+  }
+  return value;
 };
 
-const callApi = (url = '', options = {}) => {
+const callApi = (url = LOCATION_ORIGIN, options = defaultFetchHeaders) => {
   // TODO: implement
+  if (url[0] == "/") url = LOCATION_ORIGIN + url;
+  const response = fetch(url, options);
+  return response.then(
+    res => {
+      return {
+        'resp': res,
+        'json': res.status != 204 ? normalizeCasing(JSON.parse(res.body)) : null,
+      }
+    }
+  );
+  ;
 };
 
 export default callApi;
